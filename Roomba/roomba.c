9fb1fbf35c34a89817b36fd97b7ced017f961a31
Josh Simon <jsimon@students.rzjhs.org>
2018-02-11 15:00:56 -0600

    fixed for roomba

--- Roomba/roomba.c
+++ Roomba/roomba.c
@@ -1,65 +1,62 @@
 #include <kipr/botball.h>
 #include <math.h>
-double matrix[] = {0,0,0};
-int movementPorts[] = {0,0};
-int turnPorts[2] = {}; //{left,right}
-void move(double distance, double speed = 1)
+typedef int bool;
+#define true 1
+#define false 0
+double pos[] = {0,0,0};
+double PI = 3.141592;
+void move(double distance, double speed)
 {
-  for(int i = 0;i<movementPorts.Length;i++) {
-    mrp(movementPorts[i],speed,distance);
-  };
-  matrix[0] += cos(matrix[2]*PI)*distance;
-  matrix[1] += sin(matrix[2]*PI)*distance;
+  create_drive_direct(speed,speed);
+  msleep(distance/speed);
+  create_stop();
+  pos[0] += cos(pos[2]*PI)*distance;
+  pos[1] += sin(pos[2]*PI)*distance;
 }
-void turn(double radians, double speed = 1)//the radians variable is the radians divided by pi
+void turn(double radians)//the radians variable is the radians divided by PI
 {
-  for(int i = 0;i<turnPorts.Length;i++) {
-    mult = 1;
-    if(i==0) {
-      mult = -1;
-    };
-    mrp(movementPorts[i],speed*mult,radians/2);
-  };
-  matrix[2] += radians;
-  if(matrix[2] > 2) {
-    matrix[2] -= 2;
-  }
-  if(matrix[2] < 0) {
-    matrix[2] += 2;
-  }
+  set_create_total_angle(radians*180);
+  pos[2] += radians;
 }
 bool getButtonState(int buttonId)
 {
-  printf(digital(buttonId));
+  printf("%i",digital(buttonId));
   return false;
 }
-void turnTo(double radians, double speed = 1)//the radians variable is the radians divided by pi
+void turnTo(double radians)//the radians variable is the radians divided by PI
 {
-  turn(radians-matrix[2],speed);
-  matrix[2] = radians;
+  turn(radians-pos[2]);
+  pos[2] = radians;
 }
-void moveTo(double x, double y, double speed = 1)
+void moveTo(double x, double y, double speed)
 {
-  turnTo(atan2(y-matrix[1],x-matrix[0])/PI);
-  double distX = x-matrix[0];
-  double distY = y-matrix[1];
+  turnTo(atan2(y-pos[1],x-pos[0]));
+  double distX = x-pos[0];
+  double distY = y-pos[1];
   move(sqrt(distX*distX+distY*distY),speed);
-  matrix[0] = x;
-  matrix[1] = y;
+  pos[0] = x;
+  pos[1] = y;
 }
-void resetMatrix()
+void resetPos()
 {
-  matrix = {0,0,0};
+  pos[0] = 0;
+  pos[1] = 0;
+  pos[2] = 0;
 }
-void setMatrix(double m[])
+void setPos(double m[])
 {
-  matrix = m;
+  pos[0] = m[0];
+  pos[1] = m[1];
+  pos[2] = m[2];
 }
-double[] getMatrix()
+/*double[] getPos()
 {
-  return matrix;
-}
+  return pos;
+}*/
 int main()
 {
+  create_connect();
+  move(10,1);
+  create_disconnect();
   return 0;
 }
