8c60c35159ba165c37d513fb210973792629927c
Josh Simon <jsimon@students.rzjhs.org>
2018-02-14 16:43:51 -0600

    fixed the movement functions

--- Roomba/roomba.c
+++ Roomba/roomba.c
@@ -1,36 +1,33 @@
 #include <kipr/botball.h>
 #include <math.h>
-typedef int bool;
-#define true 1
-#define false 0
 double pos[] = {0,0,0};
 double PI = 3.141592;
 void move(double distance, double speed)
 {
   create_drive_direct(speed,speed);
-  msleep(distance/speed);
+  msleep(distance*1000/speed);
   create_stop();
   pos[0] += cos(pos[2]*PI)*distance;
   pos[1] += sin(pos[2]*PI)*distance;
 }
-void turn(double radians)//the radians variable is the radians divided by PI
+void turn(double radians, double speed)//the radians variable is the radians divided by PI
 {
-  set_create_total_angle(radians*180);
+  if(radians<0) {
+    radians *= -1;
+    speed *= -1;
+  }
+  create_spin_block(speed,radians*180);
+  create_stop();
   pos[2] += radians;
 }
-bool getButtonState(int buttonId)
+void turnTo(double radians, double speed)//the radians variable is the radians divided by PI
 {
-  printf("%i",digital(buttonId));
-  return false;
-}
-void turnTo(double radians)//the radians variable is the radians divided by PI
-{
-  turn(radians-pos[2]);
+  turn(radians-pos[2],speed);
   pos[2] = radians;
 }
 void moveTo(double x, double y, double speed)
 {
-  turnTo(atan2(y-pos[1],x-pos[0]));
+  turnTo(atan2(y-pos[1],x-pos[0]),100);
   double distX = x-pos[0];
   double distY = y-pos[1];
   move(sqrt(distX*distX+distY*distY),speed);
@@ -49,14 +46,11 @@ void setPos(double m[])
   pos[1] = m[1];
   pos[2] = m[2];
 }
-/*double[] getPos()
-{
-  return pos;
-}*/
 int main()
 {
   create_connect();
-  move(10,1);
+  turn(0.5,100);
+  turn(-0.5,100);
   create_disconnect();
   return 0;
 }
