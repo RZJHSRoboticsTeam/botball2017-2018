3956275b91abbc8c3430b7413281312e7f0e48fb
Josh Simon <jsimon@students.rzjhs.org>
2018-03-16 09:16:40 -0500

    changed code

--- Roomba/roomba.c
+++ Roomba/roomba.c
@@ -8,16 +8,16 @@ double PI = 3.141592;
 unsigned int gatePort = 0;
 
 int gateOpen = 680;
-int gateClosed = 1540;
+int gateClosed = 1560;
 
 void openGate()
 {
-  setServoPosition(gatePort,gateOpen);
+  set_servo_position(gatePort,gateOpen);
 }
 
 void closeGate()
 {
-  setServoPosition(gatePort,gateClosed);
+  set_servo_position(gatePort,gateClosed);
 }
 
 unsigned int lineSensorId = 0;
@@ -32,7 +32,7 @@ double mValue = 0.0;
 void goToLine(double Speed) {
     double whiteValue = analog(lineSensorId);
     create_drive_direct(Speed,Speed);
-    while(dabs(whiteValue-analog(lineSensorId))<100) {
+    while(dabs(whiteValue-analog(lineSensorId))<10) {
         msleep(10);
     };
     create_stop();
@@ -56,8 +56,8 @@ void followLine(double time, double maxSpeed, double minSpeed, bool rMode) {
             rSpeed = defL;
         };
         create_drive_direct(lSpeed,rSpeed);
-        msleep(1);
-        t+=0.001;
+        msleep(10);
+        t+=0.01;
     };
     create_stop();
 }
@@ -77,7 +77,7 @@ void turn(double radians, double speed)//the radians variable is the radians div
         radians *= -1;
         speed *= -1;
     };
-    create_spin_block(speed,radians*180);
+    create_spin_block(radians,speed);
     create_stop();
     pos[2] += radians;
 }
@@ -90,7 +90,7 @@ void turnTo(double radians, double speed)//the radians variable is the radians d
 
 void moveTo(double x, double y, double speed)
 {
-    turnTo(atan2(y-pos[1],x-pos[0])/PI,100);
+    turnTo(atan2(y-pos[1],x-pos[0])*180/PI,100);
     double distX = x-pos[0];
     double distY = y-pos[1];
     move(sqrt(distX*distX+distY*distY),speed);
@@ -110,28 +110,53 @@ void setPos(double m[])
     pos[1] = m[1];
     pos[2] = m[2];
 }
-void code()
+
+double dropOffDistance = 0;
+double backUpDistance = 0;
+
+void dropOffPoms()
 {
     goToLine(100);
-    followLine(2, 75, -25, false);
-    followLine(10, 100, 0, true);
-    /*
-    closeGate();
-    move(distance1,100);
-    goToLine(100);
-    turn(-0.5,100);
-    followLine(time,maxSpeed,minSpeed,false);
-    turn(-0.5,100);
     openGate();
+    move(dropOffDistance,100);
+    move(dropOffDistance+backUpDistance,-100);
+    closeGate();
+}
+void code()
+{
+    //goToLine(100);
+    //followLine(2, 75, -25, false);
+    //followLine(10, 100, 0, true);
+    closeGate();
+    move(120,100);
     goToLine(100);
+    move(120,100);
+    goToLine(100);
+    //turn(45,100);
+    /*goToLine(100);
+    followLine(5,100,0,false);
+    turn(0.5,100);
+    dropOffPoms();*/
+    /*
+    turn(0.5,100);
+    dropOffPoms();
+    turn(1,100);
+    goToLine(100);
+    turn(0.5,100);
+    followLine(time2,maxSpeed,minSpeed,false);
+    turn(0.5,100);
     move(distance2,100);
-    move(distance2,100);
+    turn(0.5,100);
+    goToLine(100);
+    move(distance3,-100);
+    turn(-0.5,100);
+    dropOffPoms();
     */
 }
 int main()
 {
     create_connect();
-    waitForLight(1);
+    //waitForLight(1);
     code();
     create_disconnect();
     return 0;
